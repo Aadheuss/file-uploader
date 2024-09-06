dbb5fd84e09b55fb22530991ac7ea62a70e40de2
Aadheuss <aadheusx@gmail.com>
2024-09-06 23:33:14 +0700

    Hash password on populate db

--- db/populatedb.js
+++ db/populatedb.js
@@ -1,16 +1,24 @@
 const { PrismaClient } = require("@prisma/client");
-
 const prisma = new PrismaClient();
+const bcrypt = require("bcryptjs");
 
 async function main() {
   console.log("seeding...");
 
-  await prisma.user.createMany({
-    data: [
-      { username: "Aadheuss", password: "12345678" },
-      { username: "sunflower", password: "12345678" },
-    ],
-    skipDuplicates: true,
+  await prisma.user.deleteMany();
+
+  bcrypt.hash("12345678", 10, async (err, hashedPassword) => {
+    if (err) {
+      console.error(err);
+    }
+
+    await prisma.user.createMany({
+      data: [
+        { username: "Aadheuss", password: hashedPassword },
+        { username: "sunflower", password: hashedPassword },
+      ],
+      skipDuplicates: true,
+    });
   });
 
   console.log("done");
