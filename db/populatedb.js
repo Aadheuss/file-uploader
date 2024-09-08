61928a76f4ccaabce5b6b3b7929e37ba8ad1a306
Aadheuss <aadheusx@gmail.com>
2024-09-08 09:28:21 +0700

    Add a folder named main when creating and user
    - create user with one folder named main
    - add a function to handle deleting users with related records

--- db/populatedb.js
+++ db/populatedb.js
@@ -2,24 +2,88 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 const bcrypt = require("bcryptjs");
 
+const cascadeDelete = async () => {
+  const users = await prisma.user.findMany();
+
+  console.log("starting deletion...");
+
+  for (const user of users) {
+    console.log(`deleting user ${user.username}...`);
+
+    const folders = await prisma.folder.findMany({
+      where: {
+        ownerId: user.id,
+      },
+    });
+
+    const foldersId = folders.map((folder) => folder.id);
+
+    const deleteFiles = prisma.file.deleteMany({
+      where: {
+        folderId: {
+          in: foldersId,
+        },
+      },
+    });
+
+    const deleteFolders = prisma.folder.deleteMany({
+      where: {
+        ownerId: user.id,
+      },
+    });
+
+    const deleteUser = prisma.user.delete({
+      where: {
+        id: user.id,
+      },
+    });
+
+    const transaction = await prisma.$transaction([
+      deleteFiles,
+      deleteFolders,
+      deleteUser,
+    ]);
+
+    console.log("deletion completed");
+  }
+};
+
 async function main() {
   console.log("seeding...");
 
-  await prisma.user.deleteMany();
+  await cascadeDelete();
 
-  bcrypt.hash("12345678", 10, async (err, hashedPassword) => {
-    if (err) {
-      console.error(err);
-    }
+  const users = [
+    {
+      username: "aadheuss",
+      password: "12345678",
+    },
+    {
+      username: "sunflower",
+      password: "12345678",
+    },
+  ];
 
-    await prisma.user.createMany({
-      data: [
-        { username: "Aadheuss", password: hashedPassword },
-        { username: "sunflower", password: hashedPassword },
-      ],
-      skipDuplicates: true,
+  for (const user of users) {
+    const { username, password } = user;
+    bcrypt.hash(password, 10, async (err, hashedPassword) => {
+      if (err) {
+        console.error(err);
+      }
+
+      await prisma.user.create({
+        data: {
+          username: username,
+          password: hashedPassword,
+          folders: {
+            create: {
+              name: "main",
+            },
+          },
+        },
+      });
     });
-  });
+  }
 
   console.log("done");
 }
